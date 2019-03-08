25e86abdd92600f5f0d72b86ac4b3db8550e8007
Ephraim Kigamba <ekigamba@ona.io>
2019-03-08 13:42:17 +0300

    Add UI for p2p sync mode select
    - Receive button click shows a dialog notifying the user that we are waiting for a device to connect
    - Enable cancel button to cancel the dialog
    - Send and receive buttons are disabled when the dialog is shown

---
+++ p2p-sync/src/main/java/org/smartregister/p2p/activity/P2pModeSelectActivity.java
@@ -0,0 +1,74 @@
+package org.smartregister.p2p.activity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import org.smartregister.p2p.R;
+import org.smartregister.p2p.contract.P2pModeSelectContract;
+import org.smartregister.p2p.dialog.StartReceiveModeProgressDialog;
+import org.smartregister.p2p.presenter.P2pModeSelectPresenter;
+
+public class P2pModeSelectActivity extends AppCompatActivity implements P2pModeSelectContract.View {
+
+    private Button sendButton;
+    private Button receiveButton;
+
+    private P2pModeSelectContract.Presenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p2p_mode_select);
+
+        sendButton = findViewById(R.id.btn_p2pModeSelectActivity_send);
+        receiveButton = findViewById(R.id.btn_p2pModeSelectActivity_receive);
+
+        initializePresenter();
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.onSendButtonClicked();
+            }
+        });
+
+        receiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.onReceiveButtonClicked();
+            }
+        });
+    }
+
+    @Override
+    public void enableSendReceiveButtons(boolean enable) {
+        sendButton.setEnabled(enable);
+        receiveButton.setEnabled(enable);
+    }
+
+    @Override
+    public void showReceiveProgressDialog(@NonNull DialogCancelCallback dialogCancelCallback) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        StartReceiveModeProgressDialog newFragment = new StartReceiveModeProgressDialog();
+        newFragment.setDialogCancelCallback(dialogCancelCallback);
+
+        newFragment.show(fragmentManager, "dialog_receive_progress");
+    }
+
+    @NonNull
+    @Override
+    public Context getContext() {
+        return this;
+    }
+
+    @Override
+    public void initializePresenter() {
+        presenter = new P2pModeSelectPresenter(this);
+    }
+}
--- sample/src/main/java/org/smartregister/p2p/sample/MainActivity.java
+++ sample/src/main/java/org/smartregister/p2p/sample/MainActivity.java
@@ -1,7 +1,10 @@
 package org.smartregister.p2p.sample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.smartregister.p2p.activity.P2pModeSelectActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, P2pModeSelectActivity.class));
     }
 }
