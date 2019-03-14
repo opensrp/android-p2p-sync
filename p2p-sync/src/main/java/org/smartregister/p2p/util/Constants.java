5bbc76ef0584c3c60d1624c9bf9b074f9b042c87
Ephraim Kigamba <ekigamba@ona.io>
2019-03-14 11:34:56 +0300

    Unregister click listener onStop
    - Change Constants class to interface instead of abstract class

--- p2p-sync/src/main/java/org/smartregister/p2p/util/Constants.java
+++ p2p-sync/src/main/java/org/smartregister/p2p/util/Constants.java
@@ -6,12 +6,12 @@ import com.google.android.gms.nearby.connection.Strategy;
  * Created by Ephraim Kigamba - ekigamba@ona.io on 11/03/2019
  */
 
-public abstract class Constants {
+public interface Constants {
 
-    public static final Strategy STRATEGY = Strategy.P2P_STAR;
+    Strategy STRATEGY = Strategy.P2P_STAR;
 
-    public abstract static class RQ_CODE {
-        public static final int PERMISSIONS = 2;
-        public static final int LOCATION_SETTINGS = 3;
+    interface RQ_CODE {
+        int PERMISSIONS = 2;
+        int LOCATION_SETTINGS = 3;
     }
 }
