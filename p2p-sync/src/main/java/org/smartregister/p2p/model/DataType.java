2cc50625837648dfc3ddf4bde49e000492a1d5d9
Ephraim Kigamba <ekigamba@ona.io>
2019-03-28 14:20:46 +0300

    Create Interface for sending & retrieving data to be sent
    Fixes #10
    Fixes #11

---
+++ p2p-sync/src/main/java/org/smartregister/p2p/model/DataType.java
@@ -0,0 +1,48 @@
+package org.smartregister.p2p.model;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 28/03/2019
+ */
+
+public class DataType implements Comparable<DataType> {
+
+    private final String name;
+    private final Type type;
+    private final int position;
+
+    public DataType(@NonNull String name, @NonNull Type type, int position) {
+        this.name = name;
+        this.type = type;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public int compareTo(@NonNull DataType o) {
+        if (getPosition() == o.getPosition()) {
+            return 0;
+        } else if (getPosition() < o.getPosition()) {
+            return 1;
+        }
+
+        return -1;
+    }
+
+    public enum Type {
+        MEDIA,
+        NON_MEDIA
+    }
+}
