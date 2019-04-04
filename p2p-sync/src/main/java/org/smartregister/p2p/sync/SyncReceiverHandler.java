b994a6be18e82e302289fa80d37433ff5441d86b
Ephraim Kigamba <ekigamba@ona.io>
2019-04-04 18:41:17 +0300

    Add tests for SyncReceiverHandler
    This also enables receiving another data payload while the last is still being processed by the host application

--- p2p-sync/src/main/java/org/smartregister/p2p/sync/SyncReceiverHandler.java
+++ p2p-sync/src/main/java/org/smartregister/p2p/sync/SyncReceiverHandler.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 
 import com.google.android.gms.nearby.connection.Payload;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonParseException;
 
 import org.json.JSONArray;
@@ -20,6 +19,7 @@ import org.smartregister.p2p.tasks.GenericAsyncTask;
 import org.smartregister.p2p.tasks.Tasker;
 import org.smartregister.p2p.util.SyncDataConverterUtil;
 
+import java.util.HashMap;
 import java.util.concurrent.Callable;
 
 import timber.log.Timber;
@@ -31,9 +31,8 @@ import timber.log.Timber;
 public class SyncReceiverHandler {
 
     private P2pModeSelectContract.ReceiverPresenter receiverPresenter;
-    private String awaitingPayloadId;
     private boolean awaitingManifestReceipt = true;
-    private SyncPackageManifest awaitingPayloadManifest;
+    private HashMap<Long, SyncPackageManifest> awaitingPayloadManifests = new HashMap<>();
 
     public SyncReceiverHandler(@NonNull P2pModeSelectContract.ReceiverPresenter receiverPresenter) {
         this.receiverPresenter = receiverPresenter;
@@ -52,30 +51,30 @@ public class SyncReceiverHandler {
     public void processManifest(@NonNull String endpointId, @NonNull Payload payload) {
         if (payload.getType() == Payload.Type.BYTES && payload.asBytes() != null) {
             try {
-                awaitingPayloadManifest = new Gson().fromJson(new String(payload.asBytes()), SyncPackageManifest.class);
+                SyncPackageManifest syncPackageManifest = new Gson().fromJson(new String(payload.asBytes()), SyncPackageManifest.class);;
+                awaitingPayloadManifests.put(syncPackageManifest.getPayloadId(), syncPackageManifest);
+
                 awaitingManifestReceipt = false;
             } catch (JsonParseException e) {
-                Timber.e(e, "Received invalid manifest from endpoint %s", endpointId);
+                Timber.e(e, receiverPresenter.getView().getString(R.string.log_received_invalid_manifest_from_endpoint), endpointId);
             }
         } else {
-            Timber.e(new Exception(), "Received invalid manifest from endpoint %s", endpointId);
+            Timber.e(new Exception(), receiverPresenter.getView().getString(R.string.log_receive_invalid_manifest_from_endpoint), endpointId);
         }
     }
 
     public void processRecords(@NonNull String endpointId, @NonNull Payload payload) {
-        if (awaitingPayloadManifest != null) {
-            if (payload.getId() == awaitingPayloadManifest.getPayloadId()) {
-                if (awaitingPayloadManifest.getDataType().getType() == DataType.Type.NON_MEDIA) {
-                    processNonMediaData(payload);
-                } else {
-                    processMediaData(payload);
-                }
+        if (awaitingPayloadManifests.containsKey(payload.getId())) {
+            awaitingManifestReceipt = true;
+            SyncPackageManifest payloadManifest = awaitingPayloadManifests.get(payload.getId());
+
+            if (payloadManifest.getDataType().getType() == DataType.Type.NON_MEDIA) {
+                processNonMediaData(payload);
             } else {
-                Timber.e("Ignoring payload %d since it does not match the expected payload %d"
-                        , payload.getId(), awaitingPayloadManifest.getPayloadId());
+                processMediaData(payload);
             }
         } else {
-            Timber.e(new Exception(), "Received a payload without the manifest");
+            Timber.e(new Exception(), receiverPresenter.getView().getString(R.string.log_received_payload_without_corresponding_manifest));
         }
     }
 
@@ -85,10 +84,11 @@ public class SyncReceiverHandler {
             public Long call() throws Exception {
                 JSONArray jsonArray = new Gson()
                         .fromJson(SyncDataConverterUtil.readInputStreamAsString(payload.asStream().asInputStream()), JSONArray.class);
+                SyncPackageManifest syncPackageManifest = awaitingPayloadManifests.get(payload.getId());
                 long lastRecordId = P2PLibrary.getInstance().getReceiverTransferDao()
-                        .receiveJson(awaitingPayloadManifest.getDataType(), jsonArray);
+                        .receiveJson(syncPackageManifest.getDataType(), jsonArray);
 
-                updateLastRecord(lastRecordId);
+                updateLastRecord(syncPackageManifest.getDataType().getName(),lastRecordId);
                 return lastRecordId;
             }
         }, new GenericAsyncTask.OnFinishedCallback<Long>() {
@@ -96,6 +96,7 @@ public class SyncReceiverHandler {
             public void onSuccess(@Nullable Long result) {
                 if (result != null) {
                     // We should save the last ID here and probably keep track of the next batch that we are to receive
+                    awaitingPayloadManifests.remove(payload.getId());
                 } else {
                     Timber.e(receiverPresenter.getView().getString(R.string.log_error_occurred_processing_media_data));
                     stopTransferAndReset();
@@ -110,20 +111,19 @@ public class SyncReceiverHandler {
         });
     }
 
-    private void updateLastRecord(long lastRecordId) {
+    private void updateLastRecord(@NonNull String entityName, long lastRecordId) {
         SendingDevice sendingDevice = receiverPresenter.getSendingDevice();
         if (sendingDevice != null) {
-            String entityType = awaitingPayloadManifest.getDataType().getName();
             P2pReceivedHistoryDao p2pReceivedHistoryDao = P2PLibrary.getInstance().getDb()
                     .p2pReceivedHistoryDao();
 
             P2pReceivedHistory receivedHistory = p2pReceivedHistoryDao
-                    .getHistory(sendingDevice.getDeviceId(), entityType);
+                    .getHistory(sendingDevice.getDeviceId(), entityName);
 
             if (receivedHistory == null) {
                 receivedHistory = new P2pReceivedHistory();
                 receivedHistory.setSendingDeviceId(sendingDevice.getDeviceId());
-                receivedHistory.setEntityType(entityType);
+                receivedHistory.setEntityType(entityName);
                 receivedHistory.setLastRecordId(lastRecordId);
 
                 p2pReceivedHistoryDao
@@ -141,11 +141,12 @@ public class SyncReceiverHandler {
         Tasker.run(new Callable<Long>() {
             @Override
             public Long call() throws Exception {
+                SyncPackageManifest syncPackageManifest = awaitingPayloadManifests.get(payload.getId());
                 long lastRecordId = P2PLibrary.getInstance().getReceiverTransferDao()
-                        .receiveMultimedia(awaitingPayloadManifest.getDataType(), payload.asStream().asInputStream());
+                        .receiveMultimedia(syncPackageManifest.getDataType(), payload.asStream().asInputStream());
 
                 if (lastRecordId > -1) {
-                    updateLastRecord(lastRecordId);
+                    updateLastRecord(syncPackageManifest.getDataType().getName(),lastRecordId);
                     return lastRecordId;
                 } else {
                     return null;
@@ -156,6 +157,7 @@ public class SyncReceiverHandler {
             public void onSuccess(@Nullable Long result) {
                 if (result != null) {
                     // We should save the last ID here and probably keep track of the next batch that we are to receive
+                    awaitingPayloadManifests.remove(payload.getId());
                 } else {
                     Timber.e(receiverPresenter.getView().getString(R.string.log_error_occurred_processing_non_media_data));
                     // We should not continue
---
+++ p2p-sync/src/test/java/org/smartregister/p2p/sync/SyncReceiverHandlerTest.java
@@ -0,0 +1,252 @@
+package org.smartregister.p2p.sync;
+
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.stubbing.Answer;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.ReflectionHelpers;
+import org.smartregister.p2p.P2PLibrary;
+import org.smartregister.p2p.authorizer.P2PAuthorizationService;
+import org.smartregister.p2p.contract.P2pModeSelectContract;
+import org.smartregister.p2p.model.DataType;
+import org.smartregister.p2p.model.dao.ReceiverTransferDao;
+import org.smartregister.p2p.model.dao.SenderTransferDao;
+import org.smartregister.p2p.shadows.ShadowAppDatabase;
+import org.smartregister.p2p.shadows.ShadowTasker;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 04/04/2019
+ */
+
+@RunWith(RobolectricTestRunner.class)
+@Config(shadows = {ShadowAppDatabase.class, ShadowTasker.class})
+public class SyncReceiverHandlerTest {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    private SyncReceiverHandler syncReceiverHandler;
+    @Mock
+    private P2pModeSelectContract.ReceiverPresenter receiverPresenter;
+
+    @Mock
+    private P2pModeSelectContract.View view;
+
+    @Mock
+    private P2PAuthorizationService authorizationService;
+    @Mock
+    private SenderTransferDao senderTransferDao;
+    @Mock
+    private ReceiverTransferDao receiverTransferDao;
+
+    private TreeSet<DataType> dataSyncOrder;
+
+    private DataType event = new DataType("event", DataType.Type.NON_MEDIA, 1);
+    private DataType client = new DataType("client", DataType.Type.NON_MEDIA, 2);
+    private DataType profilePic = new DataType("profile-pic", DataType.Type.MEDIA, 3);
+
+    @Before
+    public void setUp() throws Exception {
+        P2PLibrary.init(new P2PLibrary.Options(RuntimeEnvironment.application, "some password", "username"
+                , authorizationService, receiverTransferDao, senderTransferDao));
+
+        dataSyncOrder = new TreeSet<>();
+        dataSyncOrder.add(profilePic);
+        dataSyncOrder.add(client);
+        dataSyncOrder.add(event);
+
+        Mockito.doReturn(view)
+                .when(receiverPresenter)
+                .getView();
+
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                int resId = invocation.getArgument(0);
+                return RuntimeEnvironment.application.getString(resId);
+            }
+        })
+                .when(view)
+                .getString(Mockito.anyInt());
+
+        syncReceiverHandler = Mockito.spy(new SyncReceiverHandler(receiverPresenter));
+    }
+
+    @Test
+    public void processPayloadShouldCallProcessManifestWhenAwaitingManifestReceiptIsTrue() {
+        String endpointId = "id";
+        long payloadId = 89834l;
+        Payload payload = Mockito.mock(Payload.class);
+
+        SyncPackageManifest syncPackageManifest = new SyncPackageManifest(payloadId, "json", event);
+
+        Mockito.doReturn(payloadId)
+                .when(payload)
+                .getId();
+
+        Mockito.doReturn(new Gson().toJson(syncPackageManifest).getBytes())
+                .when(payload)
+                .asBytes();
+
+        Mockito.doReturn(Payload.Type.BYTES)
+                .when(payload)
+                .getType();
+
+        syncReceiverHandler.processPayload(endpointId, payload);
+
+        Mockito.verify(syncReceiverHandler, Mockito.times(1))
+                .processManifest(ArgumentMatchers.eq(endpointId), ArgumentMatchers.eq(payload));
+
+        assertFalse((boolean) ReflectionHelpers.getField(syncReceiverHandler, "awaitingManifestReceipt"));
+    }
+
+    @Test
+    public void processPayloadShouldCallProcessRecordsWhenAwaitingManifestReceiptIsFalse() {
+        String endpointId = "id";
+        Payload payload = Mockito.mock(Payload.class);
+
+        ReflectionHelpers.setField(syncReceiverHandler, "awaitingManifestReceipt", false);
+
+        syncReceiverHandler.processPayload(endpointId, payload);
+        Mockito.verify(syncReceiverHandler, Mockito.times(1))
+                .processRecords(ArgumentMatchers.eq(endpointId), ArgumentMatchers.eq(payload));
+    }
+
+    @Test
+    public void processManifestShouldRetrieveManifestAndChangeAwaitingItemWhenPayloadIsBytes() {
+        String endpointId = "id";
+        long payloadId = 829832l;
+        Payload payload = Mockito.mock(Payload.class);
+
+        SyncPackageManifest syncPackageManifest = new SyncPackageManifest(payloadId, "json", event);
+        String jsonPackageManifest = new Gson().toJson(syncPackageManifest);
+
+        Mockito.doReturn(jsonPackageManifest.getBytes())
+                .when(payload)
+                .asBytes();
+
+        Mockito.doReturn(Payload.Type.BYTES)
+                .when(payload)
+                .getType();
+
+        Mockito.doReturn(payloadId)
+                .when(payload)
+                .getId();
+
+        syncReceiverHandler.processManifest(endpointId, payload);
+
+        assertFalse((boolean) ReflectionHelpers.getField(syncReceiverHandler, "awaitingManifestReceipt"));
+        SyncPackageManifest finalSyncPackageManifest = ((HashMap<Long, SyncPackageManifest>) ReflectionHelpers.getField(syncReceiverHandler, "awaitingPayloadManifests"))
+                .get(payloadId);
+
+        assertEquals(jsonPackageManifest, new Gson().toJson(finalSyncPackageManifest));
+    }
+
+    @Test
+    public void processManifestShouldDoNothingWhenPayloadTypeIsStream() {
+        String endpointId = "id";
+        Payload payload = Mockito.mock(Payload.class);
+
+        Mockito.doReturn(Payload.Type.STREAM)
+                .when(payload)
+                .getType();
+
+        syncReceiverHandler.processManifest(endpointId, payload);
+
+        assertTrue((boolean) ReflectionHelpers.getField(syncReceiverHandler, "awaitingManifestReceipt"));
+    }
+
+    @Test
+    public void processRecordsShouldCallProcessNonMediaDataWhenPayloadCorrespondsToAwaitingManifestAndDataTypeIsNonMedia() {
+        String endpointId = "id";
+        long payloadId = 923823l;
+        Payload payload = Mockito.mock(Payload.class);
+
+        JSONArray jsonArray = new JSONArray();
+
+        Payload.Stream payloadStream = Mockito.mock(Payload.Stream.class);
+
+        Mockito.doReturn(new ByteArrayInputStream(new Gson().toJson(jsonArray).getBytes()))
+                .when(payloadStream)
+                .asInputStream();
+
+        SyncPackageManifest syncPackageManifest = new SyncPackageManifest(payloadId, "json", event);
+        Mockito.doReturn(payloadStream)
+                .when(payload)
+                .asStream();
+
+        Mockito.doReturn(Payload.Type.BYTES)
+                .when(payload)
+                .getType();
+
+        Mockito.doReturn(payloadId)
+                .when(payload)
+                .getId();
+
+        ((HashMap<Long, SyncPackageManifest>) ReflectionHelpers.getField(syncReceiverHandler, "awaitingPayloadManifests"))
+                .put(syncPackageManifest.getPayloadId(), syncPackageManifest);
+        syncReceiverHandler.processRecords(endpointId, payload);
+
+        Mockito.verify(receiverTransferDao, Mockito.times(1))
+                .receiveJson(ArgumentMatchers.eq(event), Mockito.any(JSONArray.class));
+        assertEquals(0, ((HashMap<Long, SyncPackageManifest>) ReflectionHelpers.getField(syncReceiverHandler, "awaitingPayloadManifests")).size());
+    }
+
+    @Test
+    public void processRecordsShouldCallProcessMediaDataWhenPayloadCorrespondsToAwaitingManifestAndDataTypeIsMedia() {
+        String endpointId = "id";
+        long payloadId = 923823l;
+        Payload payload = Mockito.mock(Payload.class);
+
+        JSONArray jsonArray = new JSONArray();
+
+        Payload.Stream payloadStream = Mockito.mock(Payload.Stream.class);
+        InputStream is = new ByteArrayInputStream(new Gson().toJson(jsonArray).getBytes());
+
+        Mockito.doReturn(is)
+                .when(payloadStream)
+                .asInputStream();
+
+        SyncPackageManifest syncPackageManifest = new SyncPackageManifest(payloadId, "json", profilePic);
+        Mockito.doReturn(payloadStream)
+                .when(payload)
+                .asStream();
+
+        Mockito.doReturn(Payload.Type.BYTES)
+                .when(payload)
+                .getType();
+
+        Mockito.doReturn(payloadId)
+                .when(payload)
+                .getId();
+
+        ((HashMap<Long, SyncPackageManifest>) ReflectionHelpers.getField(syncReceiverHandler, "awaitingPayloadManifests"))
+                .put(syncPackageManifest.getPayloadId(), syncPackageManifest);
+        syncReceiverHandler.processRecords(endpointId, payload);
+
+        Mockito.verify(receiverTransferDao, Mockito.times(1))
+                .receiveMultimedia(ArgumentMatchers.eq(profilePic), ArgumentMatchers.eq(is));
+        assertEquals(0, ((HashMap<Long, SyncPackageManifest>) ReflectionHelpers.getField(syncReceiverHandler, "awaitingPayloadManifests")).size());
+    }
+}
