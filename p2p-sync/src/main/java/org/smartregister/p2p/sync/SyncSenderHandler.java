92d67f6b0171da8c01153e26406c9200df2cc5c5
Ephraim Kigamba <ekigamba@ona.io>
2019-05-06 16:52:10 +0300

    Add tests

--- p2p-sync/src/main/java/org/smartregister/p2p/sync/SyncSenderHandler.java
+++ p2p-sync/src/main/java/org/smartregister/p2p/sync/SyncSenderHandler.java
@@ -21,7 +21,6 @@ import org.smartregister.p2p.tasks.Tasker;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.TreeSet;
