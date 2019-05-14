60aa461c746f0926500a7f73f0d67cc7df6c9548
Ephraim Kigamba <ekigamba@ona.io>
2019-05-14 17:12:34 +0300

    Add correct failure icon & remove storage permissions from sample app

--- sample/src/main/java/org/smartregister/p2p/sample/MainActivity.java
+++ sample/src/main/java/org/smartregister/p2p/sample/MainActivity.java
@@ -1,28 +1,19 @@
 package org.smartregister.p2p.sample;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import org.smartregister.p2p.activity.P2pModeSelectActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] permissions = new String[] {
-            Manifest.permission.READ_EXTERNAL_STORAGE
-            , Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        requestPermissions();
+        startActivity(new Intent(this, P2pModeSelectActivity.class));
     }
 
     @Override
@@ -30,26 +21,4 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        requestPermissions();
-    }
-
-    private void requestPermissions() {
-        if (!arePermissionsGranted()) {
-            ActivityCompat.requestPermissions(this, permissions, 8348);
-        } else {
-            startActivity(new Intent(this, P2pModeSelectActivity.class));
-        }
-    }
-
-    private boolean arePermissionsGranted() {
-        for (String permission: permissions) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
-        }
-
-        return true;
-    }
 }
