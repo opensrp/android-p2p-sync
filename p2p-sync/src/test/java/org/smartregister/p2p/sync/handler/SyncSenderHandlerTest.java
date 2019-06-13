ee6c5ce239f378e5b285f4b07288d610c7868ae2
Ephraim Kigamba <ekigamba@ona.io>
2019-06-13 18:57:44 +0300

    Fix codacy issues

--- p2p-sync/src/test/java/org/smartregister/p2p/sync/handler/SyncSenderHandlerTest.java
+++ p2p-sync/src/test/java/org/smartregister/p2p/sync/handler/SyncSenderHandlerTest.java
@@ -2,7 +2,6 @@ package org.smartregister.p2p.sync.handler;
 
 import com.google.android.gms.nearby.connection.Payload;
 import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
-import com.google.gson.JsonArray;
 
 import org.json.JSONArray;
 import org.junit.Before;
